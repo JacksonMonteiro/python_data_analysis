d8198eb6aef605ca208859bbaf6e3521b736ec19
jacksonmonteiro <infor.jackson324@gmail.com>
2024-08-30 11:56:45 -0300

    data visualization grouping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.data_loader import loadData
 from src.processing import process
-from src.visualization import plotBar, plotPie
+from src.visualization import plotAll
 
 def main(): 
     path = "assets/Clientes.xlsx"
@@ -11,9 +11,7 @@ def main():
         
         metrics = process(df)
 
-        plotBar(metrics['servicoFrequente'], 'Serviços com Mais Frequência', 'Serviço', 'Contagem')
-        plotBar(metrics['parceiroFrequente'], 'Parceiros com Mais Frequência', 'Parceiro', 'Contagem')
-        plotPie(metrics['nichoFrequente'], 'Distribuição dos Nichos')
+        plotAll(metrics)    
     except Exception as e:
         print(f"Ocorreu um erro ao executar a aplicação: {e}")
 
--- src/visualization.py
+++ src/visualization.py
@@ -1,17 +1,35 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+from tkinter import Tk, Button
 
-def plotBar(data, title, xlabel, ylabel):
-    sns.barplot(x=data.index, y = data.values)
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.xticks(rotation = 45, ha = "right")
+def plotAll(metrics):
+    plt.figure(figsize=(15, 10))
+    
+    # Gráfico de barras para Serviços
+    plt.subplot(2, 2, 1)
+    sns.barplot(x=metrics['servicoFrequente'].index, y=metrics['servicoFrequente'].values)
+    plt.title('Serviços com Mais Frequência')
+    plt.xlabel('Serviço')
+    plt.ylabel('Contagem')
+
+    # Gráfico de barras para Parceiros
+    plt.subplot(2, 2, 2)
+    sns.barplot(x=metrics['parceiroFrequente'].index, y=metrics['parceiroFrequente'].values)
+    plt.title('Parceiros com Mais Frequência')
+    plt.xlabel('Parceiro')
+    plt.ylabel('Contagem')
+
+    # Gráfico de pizza para Nichos
+    plt.subplot(2, 2, 3)
+    plt.pie(metrics['nichoFrequente'].values, labels=metrics['nichoFrequente'].index, autopct='%1.1f%%', startangle=140)
+    plt.title('Distribuição dos Nichos')
+
+    # Gráfico de barras para Prospecção
+    plt.subplot(2, 2, 4)
+    sns.barplot(x=metrics['prospeccao'].index, y=metrics['prospeccao'].values)
+    plt.title('Prospecção')
+    plt.xlabel('Tipo de Prospecção')
+    plt.ylabel('Contagem')
+
     plt.tight_layout()
-    plt.show()
-
-def plotPie(data, title):
-    plt.figure(figsize=(8, 8))
-    plt.pie(data.values, labels=data.index, autopct='%1.1f%%', startangle=140)
-    plt.title(title)
     plt.show()
